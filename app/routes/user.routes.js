6163f0bfb85302cb175c99af0c4def636ecafb82
Joseph Cruel <Joseph-Cruel@live.nmit.ac.nz>
2024-06-07 13:48:17 +1200

    setting up jwt for users

--- app/routes/user.routes.js
+++ app/routes/user.routes.js
@@ -1,3 +1,25 @@
 const { authJwt } = require('../middleware')
 const controller = require('../controllers/user.controller')
 
+module.exports = function (app) {
+    app.use((req, res, next) => {
+        res.header(
+            "Access-Control-Allow-Header",
+            "Origin, Content-Type, Accept"
+        )
+        next()
+    })
+    app.get("/api/test/all", controller.allAccess)
+
+    app.get(
+        "api/test/user",
+        [authJwt.verifyToken],
+        controller.userBoard
+    )
+
+    app.get(
+        "api/test/mod",
+        [authJwt.verifyToken, authJwt.isAdmin],
+        controller.adminBoard
+    )
+}
